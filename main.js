41bca4b819f4d6fb4f4acb992e83783aab6cfccf
Zhavlon <javlonadakhamov@gmail.com>
2021-08-01 23:45:41 +0600

    added control guide

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
-car.classList.add('car');
 
+car.classList.add('car');
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
